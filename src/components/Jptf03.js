003264be8930db54d63a6244ded8369eaac14c71
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2023-04-04 17:11:11 +0400

    Reorder

--- src/components/Jptf03.js
+++ src/components/Jptf03.js
@@ -6,35 +6,36 @@ function Jptf03(props){
         </div>
         <div>
             <h3>პროექტები</h3>
-            თენიეშვილი თეკლა - https://www.figma.com/file/cR12A4QnCluY8ZKqDt6nK7/Golobe---Travel-agency-website-(Community)?node-id=108-18842&t=gR5XBfkFqpLzxAEu-0
-            <br></br>
-            კეკიშვილი საბა - https://www.figma.com/file/EQNUlaDm4Kxv6fRUCQua8Y/Agriculture-Webflow-Website-Template-(Community)?node-id=1-430&t=rMTEX7Q2kE4Mdwai-0
-            <br></br>
-            ოსეფაიშვილი ალექსანდრე - https://www.figma.com/file/bEWyLh48E4A63T6LOXCZYb/Devpost-2.0-(Community)?node-id=1-2&t=kOpd5tTZOxp5nACr-0
+            ამბროლაძე რეზო - https://www.figma.com/file/XUt6kbvylgQV3f0fmEmPdd/Ecommerce-Web-Pages-(Free)-(Community)?node-id=0-1&t=embNVDEY15B6N5S5-0
             <br></br>
             ახალაია თემურ - https://www.figma.com/file/KiwbV0gGBzOa5Pzo7KGQqe/Client-first-Template-9---Webflow-Agency-(Community)-(Community)?node-id=0-1&t=9B3Hp20J8VJ1ptaB-0
             <br></br>
-            ამბროლაძე რეზო - https://www.figma.com/file/XUt6kbvylgQV3f0fmEmPdd/Ecommerce-Web-Pages-(Free)-(Community)?node-id=0-1&t=embNVDEY15B6N5S5-0
+            ბაიდოშვილი ლაზარე - https://www.figma.com/file/QfagKTuh0fxOnobAFiYIbf/Bachira-Online-Shop-(Community)?node-id=169-452
             <br></br>
-            ხელიძე გიორგი - https://www.figma.com/file/4rFfvsStZrj8rQHA5UaTVL/E-Commerce-Website-(Community)?node-id=0-1&t=tgV3pEK68i4TQ2ZI-0
+            თაბაგარი თაბა - https://www.figma.com/file/lYpu7kcPSHR3CVjpJrSo0s/Website-Event-Ticketing-(Community)?node-id=0-1&t=Uc8DMcztW9o9UJuE-0
             <br></br>
-            ნარინდოშვილი ივანე - https://www.figma.com/file/u7aYStsbMSMOewChzbWkyh/FoodWagon-Food-Delivery-Landing-Template-by-ThemeWagon-(Community)?node-id=342-144&t=OJ4iUvZaP3d8uPdx-0
+            თენიეშვილი თეკლა - https://www.figma.com/file/cR12A4QnCluY8ZKqDt6nK7/Golobe---Travel-agency-website-(Community)?node-id=108-18842&t=gR5XBfkFqpLzxAEu-0
+            <br></br>
+            კეკიშვილი საბა - https://www.figma.com/file/EQNUlaDm4Kxv6fRUCQua8Y/Agriculture-Webflow-Website-Template-(Community)?node-id=1-430&t=rMTEX7Q2kE4Mdwai-0
             <br></br>
             კონსტანტინე ფარფალია - https://www.figma.com/file/pHsb1fRdCbx67Qbm1pAEbq/Slab-Shop---E-Commerce-(Community)?node-id=0-1&t=QCOw1Abznf56D4EV-0
             <br></br>
             კუბლაშვილი დაჩი - https://www.figma.com/file/3ghP8rfj2zrfiuN2GlgVE0/2023-Audi-RS-e-tron%C2%AE-GT-Landing-Page-(Community)?node-id=0-1&t=P1aIOKVUHEJU1F4K-0
             <br></br>
+            კუსიანი რატი - https://www.figma.com/file/YY7ThTh5kyrtfg9Qh0kEts/DeliCook-%7C-Recipe-and-Delivery-Website-(Community)?node-id=0-1&t=tatVNtWqmLzhNk1Q-0
+            <br></br>
             ლიპარტია ნიკოლოზი - https://www.figma.com/file/65J3wD94odm2fUo8PzWO7g/Landing-Page-(Community)?node-id=101-11&t=MrQANENmBLfbbyZu-0
-            <br></br>
-            თაბაგარი თაბა - https://www.figma.com/file/lYpu7kcPSHR3CVjpJrSo0s/Website-Event-Ticketing-(Community)?node-id=0-1&t=Uc8DMcztW9o9UJuE-0
-            <br></br>
-            ბაიდოშვილი ლაზარე - https://www.figma.com/file/QfagKTuh0fxOnobAFiYIbf/Bachira-Online-Shop-(Community)?node-id=169-452
             <br></br>
             მიდელაური საბა - https://www.figma.com/file/mNCseShKqjWL1IcnFzKwlR/Website-Template-%7C-Skydrone-(Community)?node-id=304-14049&t=WW3Rhp9XNdHTNdf6-0
             <br></br>
+            ნარინდოშვილი ივანე - https://www.figma.com/file/u7aYStsbMSMOewChzbWkyh/FoodWagon-Food-Delivery-Landing-Template-by-ThemeWagon-(Community)?node-id=342-144&t=OJ4iUvZaP3d8uPdx-0
+            <br></br>
+            ოსეფაიშვილი ალექსანდრე - https://www.figma.com/file/bEWyLh48E4A63T6LOXCZYb/Devpost-2.0-(Community)?node-id=1-2&t=kOpd5tTZOxp5nACr-0
+            <br></br>
+            ხელიძე გიორგი - https://www.figma.com/file/4rFfvsStZrj8rQHA5UaTVL/E-Commerce-Website-(Community)?node-id=0-1&t=tgV3pEK68i4TQ2ZI-0
+            <br></br>
             ხუციშვილი საბა - https://www.figma.com/file/lW0gHEBYv2EAWmZuBK1jJu/Free-Travel-Design-Template-(Community)?node-id=0-1&t=j5fxE69V8kMJHiX2-0
             <br></br>
-            კუსიანი რატი - https://www.figma.com/file/YY7ThTh5kyrtfg9Qh0kEts/DeliCook-%7C-Recipe-and-Delivery-Website-(Community)?node-id=0-1&t=tatVNtWqmLzhNk1Q-0
         </div>
     </div>
 }
