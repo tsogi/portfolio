23c56d9a5fcd8664664f95d43be2cafa9bdb25ee
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2023-04-28 15:29:18 +0400

    Improve UI

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>IT Academy Step Groups</h2>
-      <a style={{marginRight: "30px"}} href='/sn124'>SN-124</a>
-      <a href='/jptf03'>JP_TF_03</a>
+      <div className="studentsLabel">My Students</div>
+      <a className='groupA' href='/sn124'>Group SN-124</a>
+      <br></br>
+      <a className="groupA" href='/jptf03'>Group JP_TF_03</a>
       <ProjectsPage />
     </div>
   );
--- src/components/Jptf03.js
+++ src/components/Jptf03.js
@@ -2,12 +2,8 @@ import "./Jptf03.css";
 
 function Jptf03(props){
     return <div className="jptf">
-        <div style={{ fontSize: "1.3rem", marginBottom: "60px" }}>
-            <span>ფეისბუქის ჯგუფი: </span>
-            <span>https://www.facebook.com/groups/638302204587060</span>
-        </div>
         <div>
-            <h3>პროექტები</h3>
+            <h3>ფინალური პროექტები</h3>
             <section class="articles">
                 <article class="profile">
                     <div class="profile-image">
@@ -17,7 +13,7 @@ function Jptf03(props){
                     {/* <small class="profile-user-handle">@elena_zoldado</small> */}
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/XUt6kbvylgQV3f0fmEmPdd/Ecommerce-Web-Pages-(Free)-(Community)?node-id=0-1&t=embNVDEY15B6N5S5-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank" class="btn btn--primary">Project</a>
+                        <a href="" target="_blank" class="btn btn--primary">Website</a>
                     </div>
                     {/* <div class="profile-links">
                         <a href="#" class="link link--icon">
@@ -35,10 +31,10 @@ function Jptf03(props){
                     <div class="profile-image">
                         <img src="/images/jptf03/temur.jpeg" />
                     </div>
-                    <h2 class="profile-username">ახალია თემურ</h2>
+                    <h2 class="profile-username">ახალაია თემურ</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/KiwbV0gGBzOa5Pzo7KGQqe/Client-first-Template-9---Webflow-Agency-(Community)-(Community)?node-id=0-1&t=9B3Hp20J8VJ1ptaB-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -48,7 +44,7 @@ function Jptf03(props){
                     <h2 class="profile-username">ბაიდოშვილი ლაზარე</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/QfagKTuh0fxOnobAFiYIbf/Bachira-Online-Shop-(Community)?node-id=169-452" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -58,7 +54,7 @@ function Jptf03(props){
                     <h2 class="profile-username">თაბაგარი თაბა</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/lYpu7kcPSHR3CVjpJrSo0s/Website-Event-Ticketing-(Community)?node-id=0-1&t=Uc8DMcztW9o9UJuE-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -68,7 +64,7 @@ function Jptf03(props){
                     <h2 class="profile-username">თენიეშვილი თეკლა</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/cR12A4QnCluY8ZKqDt6nK7/Golobe---Travel-agency-website-(Community)?node-id=108-18842&t=gR5XBfkFqpLzxAEu-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -78,7 +74,7 @@ function Jptf03(props){
                     <h2 class="profile-username">კეკიშვილი საბა</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/EQNUlaDm4Kxv6fRUCQua8Y/Agriculture-Webflow-Website-Template-(Community)?node-id=1-430&t=rMTEX7Q2kE4Mdwai-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -88,7 +84,7 @@ function Jptf03(props){
                     <h2 class="profile-username">კონსტანტინე ფარფალია</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/pHsb1fRdCbx67Qbm1pAEbq/Slab-Shop---E-Commerce-(Community)?node-id=0-1&t=QCOw1Abznf56D4EV-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -98,7 +94,7 @@ function Jptf03(props){
                     <h2 class="profile-username">კუბლაშვილი დაჩი</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/3ghP8rfj2zrfiuN2GlgVE0/2023-Audi-RS-e-tron%C2%AE-GT-Landing-Page-(Community)?node-id=0-1&t=P1aIOKVUHEJU1F4K-0" class="btn btn--primary">Figma</a>
-                        <a href="https://kublashvilidachi.github.io/firstproject" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="https://kublashvilidachi.github.io/firstproject" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -108,7 +104,7 @@ function Jptf03(props){
                     <h2 class="profile-username">კუსიანი რატი</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/YY7ThTh5kyrtfg9Qh0kEts/DeliCook-%7C-Recipe-and-Delivery-Website-(Community)?node-id=0-1&t=tatVNtWqmLzhNk1Q-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -118,7 +114,7 @@ function Jptf03(props){
                     <h2 class="profile-username">ლიპარტია ნიკოლოზი</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/65J3wD94odm2fUo8PzWO7g/Landing-Page-(Community)?node-id=101-11&t=MrQANENmBLfbbyZu-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -128,7 +124,7 @@ function Jptf03(props){
                     <h2 class="profile-username">მიდელაური საბა</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/mNCseShKqjWL1IcnFzKwlR/Website-Template-%7C-Skydrone-(Community)?node-id=304-14049&t=WW3Rhp9XNdHTNdf6-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -138,7 +134,7 @@ function Jptf03(props){
                     <h2 class="profile-username">ნარინდოშვილი ივანე</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/u7aYStsbMSMOewChzbWkyh/FoodWagon-Food-Delivery-Landing-Template-by-ThemeWagon-(Community)?node-id=342-144&t=OJ4iUvZaP3d8uPdx-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -148,7 +144,7 @@ function Jptf03(props){
                     <h2 class="profile-username">ოსეფაიშვილი ალექსანდრე</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/bEWyLh48E4A63T6LOXCZYb/Devpost-2.0-(Community)?node-id=1-2&t=kOpd5tTZOxp5nACr-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -158,7 +154,7 @@ function Jptf03(props){
                     <h2 class="profile-username">ხელიძე გიორგი</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/4rFfvsStZrj8rQHA5UaTVL/E-Commerce-Website-(Community)?node-id=0-1&t=tgV3pEK68i4TQ2ZI-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
                 <article class="profile">
@@ -168,7 +164,7 @@ function Jptf03(props){
                     <h2 class="profile-username">ხუციშვილი საბა</h2>
                     <div class="profile-actions">
                         <a target="_blank" href="https://www.figma.com/file/lW0gHEBYv2EAWmZuBK1jJu/Free-Travel-Design-Template-(Community)?node-id=0-1&t=j5fxE69V8kMJHiX2-0" class="btn btn--primary">Figma</a>
-                        <a href="" target="_blank"  class="btn btn--primary">Project</a>
+                        <a href="" target="_blank"  class="btn btn--primary">Website</a>
                     </div>
                 </article> 
             </section>
@@ -202,6 +198,10 @@ function Jptf03(props){
             <br></br>
             ხუციშვილი საბა - https://www.figma.com/file/lW0gHEBYv2EAWmZuBK1jJu/Free-Travel-Design-Template-(Community)?node-id=0-1&t=j5fxE69V8kMJHiX2-0
             <br></br> */}
+        </div>
+        <div class="facebookGroup">
+            <span>ფეისბუქის ჯგუფი: </span>
+            <span>https://www.facebook.com/groups/638302204587060</span>
         </div>
     </div>
 }
