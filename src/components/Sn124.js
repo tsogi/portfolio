ba37be368473378772fb11208e557d234320a013
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2023-05-01 12:27:36 +0400

    Add sn group data

--- src/components/Sn124.js
+++ src/components/Sn124.js
@@ -1,29 +1,185 @@
+import "./Jptf03.css";
+
 function Sn124(props){
-    return <div style={{margin: "40px"}} className="sn124">
-        <div style={{ fontSize: "1.3rem", marginBottom: "60px" }}>
+    return <div className="jptf">
+        <div>
+            <h3>ფინალური პროექტები</h3>
+            <section class="articles">
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/no_avatar.png" />
+                    </div>
+                    <h2 class="profile-username">არსენიშვილი ლაშა</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/pHsb1fRdCbx67Qbm1pAEbq/Slab-Shop---E-Commerce-(Community)?node-id=0-1&t=QCOw1Abznf56D4EV-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/nika_asatiani.jpeg" />
+                    </div>
+                    <h2 class="profile-username">ასათიანი ნიკა</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/YY7ThTh5kyrtfg9Qh0kEts/DeliCook-%7C-Recipe-and-Delivery-Website-(Community)?node-id=0-1&t=tatVNtWqmLzhNk1Q-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/no_avatar.png" />
+                    </div>
+                    <h2 class="profile-username">ბოჩიკაშვილი დემეტრე</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/YY7ThTh5kyrtfg9Qh0kEts/DeliCook-%7C-Recipe-and-Delivery-Website-(Community)?node-id=0-1&t=tatVNtWqmLzhNk1Q-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/giorgi_gogoladze.jpg" />
+                    </div>
+                    <h2 class="profile-username">გოგოლაძე გიორგი</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/3ghP8rfj2zrfiuN2GlgVE0/2023-Audi-RS-e-tron%C2%AE-GT-Landing-Page-(Community)?node-id=0-1&t=P1aIOKVUHEJU1F4K-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/nika_zoidze.jpg" />
+                    </div>
+                    <h2 class="profile-username">ზოიძე ნიკა</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/bEWyLh48E4A63T6LOXCZYb/Devpost-2.0-(Community)?node-id=1-2&t=kOpd5tTZOxp5nACr-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/raul_ismailovi.png" />
+                    </div>
+                    <h2 class="profile-username">ისმაილოვი რაულ</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/4rFfvsStZrj8rQHA5UaTVL/E-Commerce-Website-(Community)?node-id=0-1&t=tgV3pEK68i4TQ2ZI-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/saba_kalandadze.jpg" />
+                    </div>
+                    <h2 class="profile-username">კალანდაძე საბა</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/65J3wD94odm2fUo8PzWO7g/Landing-Page-(Community)?node-id=101-11&t=MrQANENmBLfbbyZu-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/no_avatar.png" />
+                    </div>
+                    <h2 class="profile-username">კაჭკაჭაშვილი გიორგი</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/u7aYStsbMSMOewChzbWkyh/FoodWagon-Food-Delivery-Landing-Template-by-ThemeWagon-(Community)?node-id=342-144&t=OJ4iUvZaP3d8uPdx-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/no_avatar.png" />
+                    </div>
+                    <h2 class="profile-username">კვარაცხელია ალიკა</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/mNCseShKqjWL1IcnFzKwlR/Website-Template-%7C-Skydrone-(Community)?node-id=304-14049&t=WW3Rhp9XNdHTNdf6-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/luka_kiknadze.jpg" />
+                    </div>
+                    <h2 class="profile-username">კიკნაძე ლუკა</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/XUt6kbvylgQV3f0fmEmPdd/Ecommerce-Web-Pages-(Free)-(Community)?node-id=0-1&t=embNVDEY15B6N5S5-0" class="btn btn--primary">Figma</a>
+                        <a href="https://kiknaa.github.io/project/" target="_blank" class="btn btn--primary">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/no_avatar.png" />
+                    </div>
+                    <h2 class="profile-username">კორკელია რობერტი</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/KiwbV0gGBzOa5Pzo7KGQqe/Client-first-Template-9---Webflow-Agency-(Community)-(Community)?node-id=0-1&t=9B3Hp20J8VJ1ptaB-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/aleko_kotashvili.jpeg" />
+                    </div>
+                    <h2 class="profile-username">კოტაშვილი ალეკო</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/cR12A4QnCluY8ZKqDt6nK7/Golobe---Travel-agency-website-(Community)?node-id=108-18842&t=gR5XBfkFqpLzxAEu-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/luka_natroshvili.jpg" />
+                    </div>
+                    <h2 class="profile-username">ნატროშვილი ლუკა</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/lW0gHEBYv2EAWmZuBK1jJu/Free-Travel-Design-Template-(Community)?node-id=0-1&t=j5fxE69V8kMJHiX2-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/elene_rukhadze.jpeg" />
+                    </div>
+                    <h2 class="profile-username">რუხაძე ელენე</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/lYpu7kcPSHR3CVjpJrSo0s/Website-Event-Ticketing-(Community)?node-id=0-1&t=Uc8DMcztW9o9UJuE-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/no_avatar.png" />
+                    </div>
+                    <h2 class="profile-username">ჩიხლაძე გიორგი</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/QfagKTuh0fxOnobAFiYIbf/Bachira-Online-Shop-(Community)?node-id=169-452" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/salome_tsagareli.jpeg" />
+                    </div>
+                    <h2 class="profile-username">ცაგარელი სალომე</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/lW0gHEBYv2EAWmZuBK1jJu/Free-Travel-Design-Template-(Community)?node-id=0-1&t=j5fxE69V8kMJHiX2-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+                <article class="profile">
+                    <div class="profile-image">
+                        <img src="/images/sn124/giorgi_tsereteli.jpg" />
+                    </div>
+                    <h2 class="profile-username">წერეთელი გიორგი</h2>
+                    <div class="profile-actions">
+                        <a target="_blank" href="https://www.figma.com/file/EQNUlaDm4Kxv6fRUCQua8Y/Agriculture-Webflow-Website-Template-(Community)?node-id=1-430&t=rMTEX7Q2kE4Mdwai-0" class="btn btn--primary">Figma</a>
+                        <a href="" target="_blank" class="btn btn--primary disabled">Website</a>
+                    </div>
+                </article> 
+            </section>
+        </div>
+        <div class="facebookGroup">
             <span>ფეისბუქის ჯგუფი: </span>
             <span>https://www.facebook.com/groups/190358860420964</span>
-        </div>
-        <div>
-            <h3>პროექტები</h3>
-            <div>არსენიშვილი ლაშა - https://www.figma.com/file/pHsb1fRdCbx67Qbm1pAEbq/Slab-Shop---E-Commerce-(Community)?node-id=0-1&t=QCOw1Abznf56D4EV-0</div>
-            <div>ასათიანი ნიკა - https://www.figma.com/file/YY7ThTh5kyrtfg9Qh0kEts/DeliCook-%7C-Recipe-and-Delivery-Website-(Community)?node-id=0-1&t=tatVNtWqmLzhNk1Q-0</div>
-            <div>ბოჩიკაშვილი დემეტრე - https://www.figma.com/file/YY7ThTh5kyrtfg9Qh0kEts/DeliCook-%7C-Recipe-and-Delivery-Website-(Community)?node-id=0-1&t=tatVNtWqmLzhNk1Q-0</div>
-            <div>გოგოლაძე გიორგი - https://www.figma.com/file/3ghP8rfj2zrfiuN2GlgVE0/2023-Audi-RS-e-tron%C2%AE-GT-Landing-Page-(Community)?node-id=0-1&t=P1aIOKVUHEJU1F4K-0</div>
-            <div>ზოიძე ნიკა - https://www.figma.com/file/bEWyLh48E4A63T6LOXCZYb/Devpost-2.0-(Community)?node-id=1-2&t=kOpd5tTZOxp5nACr-0
-კორკელია რობერტი - https://www.figma.com/file/KiwbV0gGBzOa5Pzo7KGQqe/Client-first-Template-9---Webflow-Agency-(Community)-(Community)?node-id=0-1&t=9B3Hp20J8VJ1ptaB-0</div>
-            <div>ისმაილოვი რაულ - https://www.figma.com/file/4rFfvsStZrj8rQHA5UaTVL/E-Commerce-Website-(Community)?node-id=0-1&t=tgV3pEK68i4TQ2ZI-0</div>
-            <div>კალანდაძე საბა - https://www.figma.com/file/65J3wD94odm2fUo8PzWO7g/Landing-Page-(Community)?node-id=101-11&t=MrQANENmBLfbbyZu-0</div>
-            <div>კაჭკაჭაშვილი გიორგი - https://www.figma.com/file/u7aYStsbMSMOewChzbWkyh/FoodWagon-Food-Delivery-Landing-Template-by-ThemeWagon-(Community)?node-id=342-144&t=OJ4iUvZaP3d8uPdx-0</div>
-            <div>კვარაცხელია ალიკა - https://www.figma.com/file/mNCseShKqjWL1IcnFzKwlR/Website-Template-%7C-Skydrone-(Community)?node-id=304-14049&t=WW3Rhp9XNdHTNdf6-0</div>
-            <div>კიკნაძე ლუკა - https://www.figma.com/file/XUt6kbvylgQV3f0fmEmPdd/Ecommerce-Web-Pages-(Free)-(Community)?node-id=0-1&t=embNVDEY15B6N5S5-0</div>
-            <div>კორკელია რობერტი - https://www.figma.com/file/KiwbV0gGBzOa5Pzo7KGQqe/Client-first-Template-9---Webflow-Agency-(Community)-(Community)?node-id=0-1&t=9B3Hp20J8VJ1ptaB-0</div>
-            <div>კოტაშვილი ალეკო - https://www.figma.com/file/cR12A4QnCluY8ZKqDt6nK7/Golobe---Travel-agency-website-(Community)?node-id=108-18842&t=gR5XBfkFqpLzxAEu-0</div>
-            <div>ნატროშვილი ლუკა - https://www.figma.com/file/lW0gHEBYv2EAWmZuBK1jJu/Free-Travel-Design-Template-(Community)?node-id=0-1&t=j5fxE69V8kMJHiX2-0</div>
-            <div>რუხაძე ელენე - https://www.figma.com/file/lYpu7kcPSHR3CVjpJrSo0s/Website-Event-Ticketing-(Community)?node-id=0-1&t=Uc8DMcztW9o9UJuE-0</div>
-            <div>ჩიხლაძე გიორგი - https://www.figma.com/file/QfagKTuh0fxOnobAFiYIbf/Bachira-Online-Shop-(Community)?node-id=169-452</div>
-            <div>ცაგარელი სალომე - https://www.figma.com/file/lW0gHEBYv2EAWmZuBK1jJu/Free-Travel-Design-Template-(Community)?node-id=0-1&t=j5fxE69V8kMJHiX2-0</div>
-            <div>წერეთელი გიორგი - https://www.figma.com/file/EQNUlaDm4Kxv6fRUCQua8Y/Agriculture-Webflow-Website-Template-(Community)?node-id=1-430&t=rMTEX7Q2kE4Mdwai-0</div>
         </div>
     </div>
 }
